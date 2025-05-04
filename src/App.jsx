8beab20a8eef545de961220cf151d2f1b1ae4975
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-04 13:14:25 +0530

    Add tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './Components/AppRoutes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './App.css'
 
 function App() {
   return (
