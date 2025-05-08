9cba6aa4290a335f68c62adcccb201de6aa6ec81
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-08 06:55:55 +0530

    Login UI update

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -25,15 +25,24 @@ const LoginForm = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input type="email" placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)} required />
-        <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <button type="submit">Login</button>
-      </form>
-      <p>Don't have an account? <Link to="/signup">Sign up here</Link></p>
+    <div className='min-h-screen flex flex-row items-center'>
+      <div className='min-h-screen bg-orange-600 text-white w-3/5 text-9xl font-bold text-center content-center'>
+        Redditt
+      </div>
+      <div className='text-orange-600 w-2/5 min-h-screen content-center'>
+        <div className='w-fit mx-auto'>
+          <h2 className='font-bold mb-3'>Have an account? Sign In</h2>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <form onSubmit={handleSubmit}>
+            <input className='bg-gray-200 mx-1 rounded-md px-1 mb-2' type="email" placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <br />
+            <input className='bg-gray-200 mx-1 rounded-md px-1 mb-2' type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <br />
+            <button className='font-bold bg-blue-800 text-white rounded-lg px-4 py-1 mb-16 mx-1' type="submit">Login</button>
+          </form>
+          <p>Don't have an account? <b><i><Link to="/signup">Sign up here</Link></i></b></p>
+        </div>
+      </div>
     </div>
   );
 };
