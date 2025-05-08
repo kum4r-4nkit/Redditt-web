9109c8200a0fbe8162c5a9b358f45dca4a10e00a
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-08 17:50:17 +0530

    Still pending

--- src/Components/pages/Header.jsx
+++ src/Components/pages/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logout from './Logout';
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-orange-600 text-white p-3 shadow-md">
       <nav className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-xl font-bold">Redditt</Link>
-        <Logout />
+        <Link to="/profile" className="text-xl font-bold bg-white text-orange-600 px-1.5 rounded-2xl">U</Link>
       </nav>
     </header>
   )
