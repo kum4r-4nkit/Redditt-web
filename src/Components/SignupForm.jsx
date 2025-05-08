21f5853304208bce8264390239f7fc4f1412babc
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-08 06:56:21 +0530

    Sign up UI update

--- src/Components/SignupForm.jsx
+++ src/Components/SignupForm.jsx
@@ -1,6 +1,6 @@
 // src/Components/SignupForm.jsx
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { signupAPI } from '../api/auth';
 import { toast } from 'react-toastify';
 import { useAuth } from '../hooks/useAuth';
@@ -34,15 +34,30 @@ const SignupForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Sign Up</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <input name="username" placeholder="Username" value={form.username} onChange={handleChange} required />
-      <input name="email" type="email" placeholder="Email" value={form.email} onChange={handleChange} required />
-      <input name="password" type="password" placeholder="Password" value={form.password} onChange={handleChange} required />
-      <input name="passwordConfirmation" type="password" placeholder="Confirm Password" value={form.passwordConfirmation} onChange={handleChange} required />
-      <button type="submit">Sign Up</button>
-    </form>
+
+    <div className='min-h-screen flex flex-row items-center'>
+      <div className='min-h-screen bg-orange-600 text-white w-3/5 text-9xl font-bold text-center content-center'>
+        Redditt
+      </div>
+      <div className='text-orange-600 w-2/5 min-h-screen content-center'>
+        <div className='w-fit mx-auto'>
+          <h2 className='font-bold mb-3 text-2xl'>Sign Up</h2>
+          <form onSubmit={handleSubmit}>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <input className='bg-gray-200 mx-1 rounded-md px-1 mb-2' name="username" placeholder="Username" value={form.username} onChange={handleChange} required />
+            <br />
+            <input className='bg-gray-200 mx-1 rounded-md px-1 mb-2' name="email" type="email" placeholder="Email" value={form.email} onChange={handleChange} required />
+            <br />
+            <input className='bg-gray-200 mx-1 rounded-md px-1 mb-2' name="password" type="password" placeholder="Password" value={form.password} onChange={handleChange} required />
+            <br />
+            <input className='bg-gray-200 mx-1 rounded-md px-1 mb-2' name="passwordConfirmation" type="password" placeholder="Confirm Password" value={form.passwordConfirmation} onChange={handleChange} required />
+            <br />
+            <button className='font-bold bg-blue-800 text-white rounded-lg px-4 py-1 mb-16 mx-1' type="submit">Sign Up</button>
+          </form>
+          <p>Already have an account? <b><i><Link to="/login">Login here</Link></i></b></p>
+        </div>
+      </div>
+    </div>
   );
 };
 
