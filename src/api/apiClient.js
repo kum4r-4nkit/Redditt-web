73eb814e429440be876114f800209ff6b55e2f68
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-21 16:05:43 +0530

    Change bsaeURL

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:3000',
+  baseURL: 'https://redditt.onrender.com',
 });
 
 // Attach the token to every request if available
