0fe34f4b905bff2009c864fd036d066e0c98cd52
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-22 19:17:09 +0530

    PWA integration

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthProvider.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onNeedRefresh() {
+    console.log('New content available, please refresh.')
+  },
+  onOfflineReady() {
+    console.log('App is ready to work offline.')
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
